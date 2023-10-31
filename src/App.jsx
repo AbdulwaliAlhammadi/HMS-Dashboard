a5db14e8587d7da33f74e501c9912836c5b3d357
AbdulwaliAlHammadi <alhammadiabdulwali@gmail.com>
2023-10-31 13:06:04 +0300

    add template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import MiniDrawer from './components/sidebar'
+
 function App() {
   return (
-    <div className="d-flex flex-column h-100 justify-content-center text-center">
-      <h1>Welcome to HMS Dashboard</h1>
-    </div>
+   <MiniDrawer />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { Header } from './components/Header.jsx'
-import { Footer } from './components/Footer.jsx'
-
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css' 
@@ -13,9 +10,7 @@ import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
   </React.StrictMode>,
 )
 
